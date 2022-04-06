2e139a0c28af9ff9a4e6f2a7ea6b5eaf70a96cd0
aniket sharma <48768524+thedefiantone17@users.noreply.github.com>
2022-04-06 22:59:06 +0530

    first commit

---
+++ StudentDatabase.sql
@@ -0,0 +1,9 @@
+create database studentdatabase;
+use studentdatabase;
+create table student(
+	id int primary key auto_increment,
+    name varchar(50),
+    department varchar(50),
+    country varchar(50)
+);
+insert into student values(1,'Dhiraj','B.arch','India');
